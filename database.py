2bd488cd8bd591780a1b66be26bca930e29a2a4f
SandeSonwabile <146043432+SandeSonwabile@users.noreply.github.com>
2024-05-23 03:08:35 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -18,3 +18,13 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(dict(row._mapping))
         return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+         result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    if row:
+        return row._asdict()
+    else:
+        return None
